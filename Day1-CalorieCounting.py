cddf37919ae3ea94c99460b3b07b1e572007bd6b
Jason Wang <jasonbln07@gmail.com>
2022-12-01 00:33:39 -0500

    Day 1, optimized

---
+++ Day1-CalorieCounting.py
@@ -0,0 +1,32 @@
+def part1(filePath):
+    with open(filePath) as input:
+        maximum, count = 0, 0
+        lines = input.readlines()
+        for line in lines:
+            if len(line) == 1:
+                maximum = max(maximum, count)
+                count = 0
+            else:
+                count += int(line)
+        return maximum
+
+print(part1("Day1-Input.txt"))
+
+def part2(filePath):
+    with open(filePath) as input:
+        max3, count = [0] * 3, 0
+        lines = input.readlines()
+        for line in lines:
+            if len(line) == 1:
+                if count > max3[-1]:
+                    max3[-1] = count
+                if count > max3[1]:
+                    max3[-1], max3[1] = max3[1], max3[-1]
+                if count > max3[0]:
+                    max3[0], max3[1] = max3[1], max3[0]
+                count = 0
+            else:
+                count += int(line)
+        return sum(max3)
+
+print(part2("Day1-Input.txt"))
