2aa252a6e3783f7775be5783b5e0da56f11bf78e
Jason Wang <jasonbln07@gmail.com>
2022-12-02 00:18:37 -0500

    Day 2, top 2000

---
+++ Day2-.py
@@ -0,0 +1,37 @@
+def part1(filePath):
+    shapeScore = {'A': 1, 'B': 2, 'C': 3}
+    letterMap = {'X': 'A', 'Y': 'B', 'Z': 'C'}
+    with open(filePath) as input:
+        score = 0
+        lines = input.readlines()
+        for line in lines:
+            opponent, me = line.split()
+            score += shapeScore[letterMap[me]]
+            if opponent == letterMap[me]:
+                score += 3
+            elif (opponent == 'A' and me == 'Y' or 
+                  opponent == 'B' and me == 'Z' or 
+                  opponent == 'C' and me == 'X'):
+                score += 6
+        return score
+
+print(part1("Day2-Input.txt"))
+
+def part2(filePath):
+    shapeScore = {'A': 1, 'B': 2, 'C': 3}
+    needToLose = {'B': 'A', 'C': 'B', 'A': 'C'}
+    needToWin = {'A': 'B', 'B': 'C', 'C': 'A'}
+    with open(filePath) as input:
+        score = 0
+        lines = input.readlines()
+        for line in lines:
+            opponent, need = line.split()
+            if need == 'X':
+                score += shapeScore[needToLose[opponent]]
+            elif need == 'Y':
+                score += shapeScore[opponent] + 3
+            else:
+                score += shapeScore[needToWin[opponent]] + 6
+        return score
+
+print(part2("Day2-Input.txt"))
