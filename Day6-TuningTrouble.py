45fec698ea5d9e7d2f723992907aebd2623c311d
Jason Wang <jasonbln07@gmail.com>
2022-12-06 00:27:59 -0500

    Day 6, deque is faster but more typing

---
+++ Day6-TuningTrouble.py
@@ -0,0 +1,29 @@
+def part1(filePath):
+    with open(filePath) as input:
+        lines = input.readlines()
+        window = []
+        for i, c in enumerate(lines[0]):
+            if i < 3:
+                window.append(c)
+            else: 
+                window.append(c)
+                if len(set(window)) == 4:
+                    return i + 1
+                window.pop(0)
+
+print(part1("Day6-Input.txt"))
+
+def part2(filePath):
+    with open(filePath) as input:
+        lines = input.readlines()
+        window = []
+        for i, c in enumerate(lines[0]):
+            if i < 13:
+                window.append(c)
+            else: 
+                window.append(c)
+                if len(set(window)) == 14:
+                    return i + 1
+                window.pop(0)
+                
+print(part2("Day6-Input.txt"))
